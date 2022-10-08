cd49043d479c459f6584126bfae0788f510e6411
roman-4erkasov <roman.4erkasov@gmail.com>
2022-10-08 09:56:07 +0000

    wip

--- hw02/main.c
+++ hw02/main.c
@@ -4,12 +4,12 @@
 int main(){
     int nrun = 5;
     double average_runtime, runtime;
-    // size_t n_arr[] = {500, 512, 1000, 1024, 2000, 2048};
-    size_t n_arr[] = {1, 2, 5, 10, 15, 20};
-    test_mm(n_arr, nrun, IJK, "IJK");
-    test_mm(n_arr, nrun, IKJ, "IKJ");
-    test_mm(n_arr, nrun, JIK, "JIK");
-    test_mm(n_arr, nrun, JKI, "JKI");
+    size_t n_arr[] = {500, 512, 1000, 1024, 2000, 2048};
+    // size_t n_arr[] = {1, 2, 5, 10, 15, 20};
+    // test_mm(n_arr, nrun, IJK, "IJK");
+    // test_mm(n_arr, nrun, IKJ, "IKJ");
+    //test_mm(n_arr, nrun, JIK, "JIK");
+    //test_mm(n_arr, nrun, JKI, "JKI");
     test_mm(n_arr, nrun, KIJ, "KIJ");
     test_mm(n_arr, nrun, KJI, "KJI");
     test_mv(n_arr, nrun, IJ, "IJ");
